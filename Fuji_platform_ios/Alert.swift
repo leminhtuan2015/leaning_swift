9680d7541fa5f99eb46f5d3fdb63dccb5a992091
Fuji <fuji@Fujis-iMac.local>
2017-03-06 17:16:39 +0700

    change_view

---
+++ Fuji_platform_ios/Alert.swift
@@ -0,0 +1,26 @@
+//
+//  Alert.swift
+//  Fuji_platform_ios
+//
+//  Created by Fuji on 3/6/17.
+//  Copyright © 2017 Fuji. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Alert {
+    class func confirm(viewController: UIViewController, message: String, callback: @escaping (_ isOk: Bool) -> Void){
+        let alertController = UIAlertController(title: Constant.CONFIRM, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
+            callback(true)
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction!) in
+            callback(false)
+        }))
+        
+        viewController.present(alertController, animated: true, completion: nil)
+    }
+}
--- Fuji_platform_ios/User.swift
+++ Fuji_platform_ios/User.swift
@@ -12,7 +12,14 @@ import SwiftyJSON
 
 class User {
     private static let API_LOGIN_URL: String = "\(Constant.API_URL)user/login"
-    private static var me: User = User()
+    private static let API_LOGOUT_URL: String = "\(Constant.API_URL)user/logout"
+    private static var currentUser: User? = nil
+    
+    private static let USER_NAME_JSON_KEY = "username"
+    private static let USER_PASSWORD_JSON_KEY = "password"
+    private static let PRODUCT_CODE_JSON_KEY = "productCode"
+    private static let TOKEN_JSON_KEY = "token"
+    private static let UID_JSON_KEY = "uid"
     
     private var username: String = ""
     private var password: String = ""
@@ -64,15 +71,28 @@ class User {
         self.expires_in = expires_in
     }
     
+    class func loadSession() {
+        let isLoggined = UserDefaults.standard.string(forKey: TOKEN_JSON_KEY) != nil
+        
+        if isLoggined {
+            let username = UserDefaults.standard.string(forKey: USER_NAME_JSON_KEY)!
+            let password = UserDefaults.standard.string(forKey: USER_PASSWORD_JSON_KEY)!
+            let token = UserDefaults.standard.string(forKey: TOKEN_JSON_KEY)!
+            
+            currentUser = User.init(username: username, password: password, token: token)
+            
+        }
+        
+    }
+    
     class public func login(user: User, callback: @escaping (_ isSuccess: Bool, _ message: String) -> Void){
         
         Logger.log(string: "login function")
         
         
-        let parameters : Parameters = ["username": user.getUsername(),
-                                       "password": user.getPassword(),
-                                       "productCode": Constant.PRODUCT_CODE
-                                    ]
+        let parameters : Parameters = [USER_NAME_JSON_KEY: user.getUsername(),
+                                       USER_PASSWORD_JSON_KEY: user.getPassword(),
+                                       PRODUCT_CODE_JSON_KEY: Constant.PRODUCT_CODE]
         
         
         Alamofire.request(API_LOGIN_URL, method: .post, parameters: parameters, encoding: JSONEncoding.default)
@@ -84,12 +104,16 @@ class User {
             case .success(let value):
                 
                 let json = JSON(value)
-                let token = json["token"].stringValue
+                let token = json[TOKEN_JSON_KEY].stringValue
                 
                 Logger.log(string: "Login Successful: \(token)")
                 
                 user.setToken(token: token)
-                me = user
+                currentUser = user
+                
+                UserDefaults.standard.set(currentUser?.getUsername(), forKey: USER_NAME_JSON_KEY)
+                UserDefaults.standard.set(currentUser?.getPassword(), forKey: USER_PASSWORD_JSON_KEY)
+                UserDefaults.standard.set(currentUser?.getToken(), forKey: TOKEN_JSON_KEY)
                 
                 callback(true, "OK")
                 
@@ -98,6 +122,37 @@ class User {
                 Logger.log(string: error.localizedDescription)
                 print(error)
             }
+        }
+    }
+    
+    class func isLoggedId() -> Bool {
+        return currentUser != nil
+    }
+    
+    class func logout(callback: @escaping (_ isSuccess: Bool, _ message: String) -> Void){
+        let parameters : Parameters = [TOKEN_JSON_KEY: currentUser!.getToken(),
+                                       UID_JSON_KEY: currentUser!.getUsername()]
+        
+        Alamofire.request(API_LOGOUT_URL, method: .post, parameters: parameters, encoding: JSONEncoding.default)
+            .validate().responseJSON { response in
+                
+                Logger.log(string: response.debugDescription)
+                
+                switch response.result {
+                case .success:
+                    
+                    UserDefaults.standard.removeObject(forKey: USER_NAME_JSON_KEY)
+                    UserDefaults.standard.removeObject(forKey: USER_PASSWORD_JSON_KEY)
+                    UserDefaults.standard.removeObject(forKey: TOKEN_JSON_KEY)
+                    User.currentUser = nil
+                    
+                    callback(true, "OK")
+                    
+                case .failure(let error):
+                    callback(false, "OK")
+                    
+                    Logger.log(string: error.localizedDescription)
+                }
         }
     }
     
