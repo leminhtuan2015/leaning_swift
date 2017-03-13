a412e86dc75412b89af429e6975f503c6a933b44
Fuji <fuji@Fujis-iMac.local>
2017-03-13 10:00:27 +0700

    keyboard_handing

--- Fuji_platform_ios/BasicViewController.swift
+++ Fuji_platform_ios/BasicViewController.swift
@@ -26,5 +26,31 @@ class BasicViewController: UIViewController, UITextFieldDelegate {
         let backButton = UIBarButtonItem(title: text, style:.plain, target: nil, action: nil)
         navigationItem.backBarButtonItem = backButton
     }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.animateTextField(textField: textField, up:true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        self.animateTextField(textField: textField, up:false)
+    }
+    
+    private func animateTextField(textField: UITextField, up: Bool) {
+        let movementDistance:CGFloat = -130
+        let movementDuration: Double = 0.3
+        
+        var movement:CGFloat = 0
+        if up {
+            movement = movementDistance
+        } else {
+            movement = -movementDistance
+        }
+        
+        UIView.beginAnimations("animateTextField", context: nil)
+        UIView.setAnimationBeginsFromCurrentState(true)
+        UIView.setAnimationDuration(movementDuration)
+        self.view.frame = self.view.frame.offsetBy(dx: 0, dy: movement)
+        UIView.commitAnimations()
+    }
 
 }
